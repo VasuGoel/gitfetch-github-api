6fcd1f932131ce6f96b701583c6e5002469760f6
Vasu Goel <goelvasu0712@gmail.com>
2019-08-08 18:51:23 +0530

    Create basic app.js with selector variables

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
