13266b5d5e92aab22bd966e4570fd64d97d596cd
Vasu Goel <goelvasu0712@gmail.com>
2019-08-09 18:29:31 +0530

    Success making api calls to endpoint

--- js/app.js
+++ js/app.js
@@ -1,20 +1,22 @@
-const inputValue = document.querySelector("#search");
-const searchButton = document.querySelector(".searchButton");
-const nameContainer = document.querySelector(".main__profile-name");
-const unContainer = document.querySelector(".main__profile-username");
-const reposContainer = document.querySelector(".main__profile-repos");
-const urlContainer = document.querySelector(".main__profile-url");
-
 $(document).ready(() => {
+
+    // Read client_id & client_secret form hidden file client_tokens.json
     $.getJSON( "/client_tokens.json", function(json) {
         const client_id = json.client_id;
         const client_secret = json.client_secret;
 
-        // $('.searchButton').click(() => {
-            const fetchUser = async (user) => {
-                const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
-            }
-        // })
+        // Make call to GitHub API
+        const fetchUser = async (user) => {
+            const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+            const data = api_call.json();
+            return data;    // equivalent to return {data: data}
+        };
 
+        // Fetch user data when search button is clicked
+        $('.searchButton').click(() => {
+            fetchUser($('#search').val()).then((res) => {
+                console.log(res);
+            })
+        });
     });
-})
+});
