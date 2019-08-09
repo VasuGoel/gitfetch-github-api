b425150bbec242b8269954e33d43b3b101505b3c
Vasu Goel <goelvasu0712@gmail.com>
2019-08-09 17:16:30 +0530

    Use fetch api with asyn & await

--- js/app.js
+++ js/app.js
@@ -5,10 +5,16 @@ const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
 
-var client_id = 0;
-var client_secret = 0;
+$(document).ready(() => {
+    $.getJSON( "/client_token.json", function(json) {
+        const client_id = json.client_id;
+        const client_secret = json.client_secret;
 
-$.getJSON( "/client_token.json", function(json) {
-    client_id = json.client_id;
-    client_secret = json.client_secret;
-});
+        // $('.searchButton').click(() => {
+            const fetchUser = async (user) => {
+                const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+            }
+        // })
+
+    });
+})
