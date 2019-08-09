d59a1fdc59d3c8fe87b563009e795db9dea2f4eb
Vasu Goel <goelvasu0712@gmail.com>
2019-08-09 13:02:40 +0530

    Import local json using $.getJSON()

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
+
+var client_id = 0;
+var client_secret = 0;
+
+$.getJSON( "/client_token.json", function(json) {
+    client_id = json.client_id;
+    client_secret = json.client_secret;
+});
